403dd6ad7e7f8afcb14f5b1a64a82884d9cea4df
kekemato <magdalena.brzozowska94@wp.pl>
2018-11-24 18:18:49 +0100

    made list of moves, keys in map method are missing

--- src/index.js
+++ src/index.js
@@ -47,41 +47,52 @@ class Board extends React.Component {
 
 class Game extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             history: [{
                 squares: Array(9).fill(null),
             }],
             xIsNext: true,
-        };
+        }
     }
-    
+
     handleClick(i) {
-        const history = this.state.history;
-        const current = history[history.length - 1];
+        const history = this.state.history
+        const current = history[history.length - 1]
         const squares = current.squares.slice()
         if (calculateWinner(squares) || squares[i]) {
-            return;
+            return
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O'
         this.setState({
             history: history.concat([{
                 squares: squares,
-              }]),
+            }]),
             xIsNext: !this.state.xIsNext
         })
     }
 
     render() {
-        const history = this.state.history;
-        const current = history[history.length - 1];
-        const winner = calculateWinner(current.squares);
+        const history = this.state.history
+        const current = history[history.length - 1]
+        const winner = calculateWinner(current.squares)
 
-        let status;
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start'
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
+        let status
         if (winner) {
-            status = 'Winner: ' + winner;
+            status = 'Winner: ' + winner
         } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
         }
         return (
             <div className="game">
@@ -93,10 +104,10 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
-        );
+        )
     }
 }
 
@@ -112,7 +123,7 @@ function calculateWinner(squares) {
         [2, 4, 6],
     ]
     for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
+        const [a, b, c] = lines[i]
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a]
         }
